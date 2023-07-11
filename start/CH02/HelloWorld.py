d5b55dd2991b1a7ace7a92c934f03cbf0404bc6e
leenaramzan <ramzan.leena@student.greenriver.edu>
2023-07-11 16:22:47 -0700

    Made changes

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created 
+
+your_name = input("What is your name?")
+print("Hello".format(your_name))
+print("Today is going to be a great day!")
