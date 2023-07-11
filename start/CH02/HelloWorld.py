df4dc50326f7bed4923a625efd73bde4642757a6
leenaramzan <ramzan.leena@student.greenriver.edu>
2023-07-11 16:36:45 -0700

    deleted last script

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -5,5 +5,3 @@
 your_name = input("What is your name?")
 print("Hello {0}".format(your_name))
 print("Today is going to be a great day!")
-your_age = input("What is your age?")
-print("In 2 years, you will be {0}".format(your_age plus 2))
