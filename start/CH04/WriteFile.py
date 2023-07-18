a3d06e286ac75497158858613136af7082ebcc6a
leenaramzan <ramzan.leena@student.greenriver.edu>
2023-07-18 15:05:29 -0700

    rename

---
+++ start/CH04/WriteFile.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Sample script that writes to a file
+# By 
+
+your_name = input("What is your name?")
+favorite_color = input("What is your favorite color?")
+pet_name = input("What was your first pet's name?")
+maiden_name = input("What is your mother's maiden name?")
+elementary_school = input("What elementary school did you attend?")
