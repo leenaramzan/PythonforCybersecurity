cd77c1091490d6c4997225f45b06cfb9c8005a74
leenaramzan <ramzan.leena@student.greenriver.edu>
2023-07-18 15:18:14 -0700

    made changes

--- start/CH04/ReadFile.py
+++ start/CH04/ReadFile.py
@@ -2,7 +2,7 @@
 # Sample script that reads from a file
 # By 
 
-ip_file = open("WriteFile.py", "r")
+ip_file = open("hackme.txt", "r")
 ip_addresses = ip_file.read()
 print(ip_addresses)
 ip_file.close()
