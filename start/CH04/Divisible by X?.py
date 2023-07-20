bab62f94cd27cdf0842874f872f7b0712f45a325
leenaramzan <ramzan.leena@student.greenriver.edu>
2023-07-20 16:02:15 -0700

    made a new file and added to it

---
+++ start/CH04/Divisible by X?.py
@@ -0,0 +1,7 @@
+num = int(input("What is the number:"))
+div = int(input("What is the diviser:"))
+mod = num % div
+if mod < 0:
+    print("16 is divisible")
+else:
+    print("16 is NOT divisible")
